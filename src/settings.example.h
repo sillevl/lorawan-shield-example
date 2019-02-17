4ab5566e8ce930c43b4a115b136234ae0c5a628b
Sille Van Landschoot <info@sillevl.be>
2019-02-17 16:47:10 +0100

    add settings example file

--- src/settings.example.h
+++ src/settings.example.h
@@ -2,12 +2,12 @@
 
 #include "config.h"
 
-SimpleLoRaWAN::LoRaWANKeys keys = { 
+using namespace SimpleLoRaWAN;
+
+LoRaWANKeys keys = { 
     { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },         // devEui
     { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },         // appEui
     { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 } // appKey
 };
 
-SimpleLoRaWAN::Pinmapping pins = { D11, D12, D13, A0, A1, D2, D3 };  // miso, mosi, clk, nss, reset, dio0, dio1
-
-//SimpleLoRaWAN::Config config = { keys, pins, true };
+Pinmapping pins = { D11, D12, D13, A0, A1, D2, D3 };  // miso, mosi, clk, nss, reset, dio0, dio1
